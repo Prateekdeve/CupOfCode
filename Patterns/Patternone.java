0dfa73d82f3517ebba8079fbd1544c015618d5a0
Prateekdeve <baranwalprateek20@gmail.com>
2026-08-12 19:55:14 +0530

    First pattern

---
+++ Patterns/Patternone.java
@@ -0,0 +1,29 @@
+package CupOfCode.Patterns;
+import java.util.Scanner;
+
+public class Patternone {
+  public static void main(String[] args) {
+    int n;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the value of n: ");
+    n = sc.nextInt();
+    for(int i =1 ;i <=n ;i++){
+      for(int j=1 ;j <=n ;j++){
+        System.out.print("*");
+      }
+      System.out.println(" ");
+    }
+   
+  }
+}
+
+// *****
+//*****
+// *****
+// *****
+// ****** */
+// *****
+// *****
+// *****
+// *****
+// *****
