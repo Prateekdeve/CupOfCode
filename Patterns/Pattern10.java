8d62e427a28cfcc02d498a2a66841743d461437f
Prateekdeve <baranwalprateek20@gmail.com>
2026-08-18 09:57:09 +0530

    Bow Pattern

--- Patterns/Pattern10.java
+++ Patterns/Pattern10.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Pattern10 {
   public static void main(String[] args) {
-    
+    int n ;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter n :");
+    n = sc.nextInt();
+
+    for(int i =1 ;i<= 2*n;i++){
+      // for(int j = 1 ;j<= i ;j++){
+      //   System.out.print("*");
+      // }
+      int stars;
+      if( i <= n){
+        stars = i;
+      }else{
+        stars = 2*n -i;
+      }
+      System.out.print("*".repeat(stars));
+      System.out.println(" ");
+    }
   }
 // *
 // **
