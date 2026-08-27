3064be36727f7f342e2deca8598e71974441b469
Prateekdeve <baranwalprateek20@gmail.com>
2026-08-27 13:00:33 +0530

    Butterfly Pattern

---
+++ Patterns/Pattern20.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Pattern20 {
+  public static void main(String[] args) {
+    int n;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter n:");
+    n = sc.nextInt();
+    for (int i = 1; i <= n; i++) {
+
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+
+      for (int j = 1; j <= 2 * (n - i); j++) {
+        System.out.print(" ");
+      }
+
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    /// Lower half
+    for (int i = n - 1; i >= 1; i--) {
+
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+
+      for (int j = 1; j <= 2 * (n - i); j++) {
+        System.out.print(" ");
+      }
+
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
+
+// Butterfly Pattern
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
