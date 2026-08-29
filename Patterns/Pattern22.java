09a9392205a3d32916c1c6d0949d3dde8b608999
Prateekdeve <baranwalprateek20@gmail.com>
2026-08-29 10:48:51 +0530

    Square with decreasing values of n

---
+++ Patterns/Pattern22.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Pattern22{
+  public static void main(String args[]){
+    int n ;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter n:");
+    n = sc.nextInt();
+    
+
+    for(int i = 0;i< (2*n)-1;i++){
+      for(int j = 0;j<(2*n)-1;j++){
+        int top = i;
+        int left = j;
+        int bottom = (2*n-2) -i;
+        int right = (2*n-2) -j;
+        
+        int minDis = Math.min(Math.min(top ,bottom) ,Math.min(left ,right));
+        int val = n -minDis;
+        System.out.print(val+" ");
+      }
+      System.out.println(" ");
+    }
+  }
+}
+
+// Difficult Target Pattern
+// 5 5 5 5 5 5 5 5 5 
+// 5 4 4 4 4 4 4 4 5 
+// 5 4 3 3 3 3 3 4 5 
+// 5 4 3 2 2 2 3 4 5 
+// 5 4 3 2 1 2 3 4 5 
+// 5 4 3 2 2 2 3 4 5 
+// 5 4 3 3 3 3 3 4 5 
+// 5 4 4 4 4 4 4 4 5 
+// 5 5 5 5 5 5 5 5 5
